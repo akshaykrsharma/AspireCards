9446bb27affc925d80116ae27c562cd7ad6ca100
Akshay Sharma <sharma.akshay5@gmail.com>
2022-02-12 14:20:41 +0530

    Implemented Lottie to show loader

--- src/components/common/LoadingView.tsx
+++ src/components/common/LoadingView.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import {Modal, ActivityIndicator, View} from 'react-native';
+import {Modal, StyleSheet, View} from 'react-native';
 import {LoaderPropsType} from '../../interfaces/interface';
+import LottieView from 'lottie-react-native';
 import Colors from '../../res/Colors';
+const lottiePath = '../../assests/loader.json';
+
 function LoadingView(props: LoaderPropsType) {
-	if (props.isLoading) {
-		return (
-			<Modal transparent={true} visible={props.isLoading}>
-				<View
-					style={{
-						flex: 1,
-						backgroundColor: 'rgba(0,0,0,0.5)',
-						justifyContent: 'center',
-					}}>
-					<ActivityIndicator size="large" color={Colors.app_theme} />
-				</View>
-			</Modal>
-		);
-	}
-	else return <></>;
+  if (props.isLoading) {
+    return (
+      <Modal transparent={true} visible={props.isLoading}>
+        <View style={styles.containerStyle}>
+          <LottieView
+            style={styles.loaderStyle}
+            source={require(lottiePath)}
+            autoPlay
+            loop
+          />
+        </View>
+      </Modal>
+    );
+  } else return <></>;
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    backgroundColor: Colors.secondary_text,
+    justifyContent: 'center',
+  },
+  loaderStyle: {
+    width: 150,
+    height: 100,
+    alignSelf: 'center'
+  }
+});
 
 export default LoadingView;
